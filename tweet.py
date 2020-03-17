0a4998cc65f5af83bb43996658aa25151515614a
Dori <dorsa.ghoreishi@gmail.com>
2020-03-17 10:09:13 -0500

    Remove dump and read json and excel output

--- tweet.py
+++ tweet.py
@@ -1,39 +1,27 @@
-import json
 import csv
 import pandas as pd
 from twython import Twython
 
+# Read credentials
 with open('./cred.txt','r') as cr:
     cred = cr.read()[:-1].split('\n')
 
+credentials = {'APP_KEY': cred[0], 'APP_SECRET': cred[1], 'ACCESS_TOKEN': cred[2], 'ACCESS_SECRET': cred[3]}
+python_tweets = Twython(credentials['APP_KEY'],credentials['APP_SECRET'])
+
+# Read state data
 states = pd.read_csv('files/States.csv')
-
-credentials = {}  
-credentials['APP_KEY'] = cred[0] 
-credentials['APP_SECRET'] =  cred[1] 
-credentials['ACCESS_TOKEN'] = cred[2] 
-credentials['ACCESS_SECRET'] = cred[3]
-
-with open("twitter_credentials.json", "w") as file:  
-    json.dump(credentials, file)
-    
-with open("twitter_credentials.json", "r") as file:  
-    creds = json.load(file)
-
-python_tweets = Twython(creds['APP_KEY'], creds['APP_SECRET'])
-
 geo = states['Geo']
 sta = states['State']
-par = states['Political Party']
 
-dict_ = {'user': [], 'date': [], 'text': [], 'id':[], 'favorite_count': [], 'retweet_num': [], 'state': [], 'party': []} 
+# Loop over states' capital and date 
+dict_ = {'user': [], 'date': [], 'text': [], 'id':[], 'favorite_count': [], 'retweet_num': [], 'state': []} 
 for i in range(0,len(geo)):
     for j in range(10,17):
         query = {'q': '#COVID-19 -filter:retweets AND -filter:replies',
                  'count': 200,
                  'lang': 'en',
                  'until': '2020-03-%s' % j,
-                 'exclude_replies': 'true',
                  'geocode': geo[i]}
     
         for status in python_tweets.search(**query)['statuses']:  
@@ -44,11 +32,7 @@ for i in range(0,len(geo)):
             dict_['favorite_count'].append(status['favorite_count'])
             dict_['retweet_num'].append(status['retweet_count'])
             dict_['state'].append(sta[i])
-            dict_['party'].append(par[i])
 
+# Save outputs as CSV
 df = pd.DataFrame(dict_)
 df.to_csv ('twitter_query.csv', index = False, header = True, quoting = csv.QUOTE_NONNUMERIC)
-
-writer = pd.ExcelWriter('twitter_query.xlsx', engine = 'xlsxwriter')
-df.to_excel(writer, sheet_name = 'Sheet1')
-writer.save()
